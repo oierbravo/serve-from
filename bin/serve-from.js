b7d8578334ff28b085360ef2e8dd5ee42696f74a
oier <oierbravo@gmail.com>
2021-02-15 13:36:10 +0100

    :sparkles: host & url query support. Changes request with axios

--- bin/serve-from.js
+++ bin/serve-from.js
@@ -2,11 +2,12 @@
 
 const package = require('../package.json');
 
-const request = require('request');
-const express = require('express')
-var cors = require('cors')
-const app = express()
-app.use(cors())
+//const request = require('request');
+const axios = require('axios');
+const express = require('express');
+var cors = require('cors');
+const app = express();
+app.use(cors());
 
 const chalk = require('chalk');
 const { Command } = require('commander');
@@ -15,21 +16,26 @@ program.version(package.version);
 
 program.requiredOption('-s, --source <source>', 'Source server without protocol.')
 .option('-p, --port <port>', 'Output port. Defaults: 8080',8080)
+.option('-h, --host <host>', 'Output host. Defaults: localhost','localhost')
 .parse(process.argv);
 
 const source = program.source;
 const port = program.port;
+const host = program.host;
+
 const sourceUrl = `https://${source}`
 
 console.log('Source host: ' + chalk.yellow(sourceUrl));
 app.get('*', async (req, res) => {
-    request(`${sourceUrl}${req.path}`, function (error, response, body) {
-        if(error) {
-          console.error('error:', error); // Print the error if one occurred
-        }
-        res.status(response.statusCode).send(body)
-    });
-  })
-  app.listen(port, () => {
-    console.log('Listening on ' + chalk.green(`http://localhost:${port}`))
+  axios.get(`${sourceUrl}${req.path}`, {params:req.query })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function (response) {
+      res.status(response.request.res.statusCode).send(response.data);
+    })
+})
+  console.log(host);
+  app.listen(port, host, () => {
+    console.log('Listening on ' + chalk.green(`http://${host}:${port}`))
   })
